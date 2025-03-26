dee00f1a158a8c194168744978f568c4c0d2a1bb
zxcdenjir <rush.wars1592@gmail.com>
2025-03-26 12:25:28 +0300

    Строковые массивы

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿int[] a = new int[21];
+﻿string[] a = new string[21];
 for (int i = 0; i < a.Length; i++)
 {
-    a[i] = i + 1;
+    a[i] = $"a{i+1}";
 }
 
-int[,] b = new int[6, 6];
+string[,] b = new string[6, 6];
+for (int i = 0; i < b.GetLength(0); i++)
+{
+    for (int j = 0; j < b.GetLength(1); j++)
+    {
+        b[i, j] = "0";
+    }
+}
+
 int last_element = 0;
 
 for (int i = 0; i < b.GetLength(1); i++)
@@ -20,43 +28,38 @@ for (int i = 0; i < b.GetLength(1); i++)
 
 Console.WriteLine("Массив A:");
 PrintArray(a);
-Console.WriteLine("\nМассив B");
+Console.WriteLine("\nМассив B:");
 PrintMatrix(b);
-Console.WriteLine("\nИнвертированный массив B");
+Console.WriteLine("\nИнвертированный массив B:");
 PrintReversedMatrix(b);
-Console.WriteLine("\nПо-другому инвертированный массив B");
+Console.WriteLine("\nПо-другому инвертированный массив B:");
 PrintReversedMatrix2(b);
 
 
 
 
-void PrintArray(int[] array)
+void PrintArray(string[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        if (i == array.Length - 1)
+            Console.Write(array[i]);
+        else
+            Console.Write(array[i] + ", ");
     }
-    Console.WriteLine();
+    Console.WriteLine("]");
 }
 
-void PrintMatrix(int[,] array)
+void PrintMatrix(string[,] array)
 {
-    int maxNumberLength = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            maxNumberLength = Math.Max(maxNumberLength, array[i, j].ToString().Length);
-        }
-    }
-
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             if (j == 0)
-                Console.Write("│ ");
-            Console.Write("{0," + maxNumberLength + "} ", array[i, j]);
+                Console.Write("│");
+            Console.Write(" {0, 3} ", array[i, j]);
             if (j == array.GetLength(1) - 1)
                 Console.Write("│");
         }
@@ -64,24 +67,15 @@ void PrintMatrix(int[,] array)
     }
 }
 
-void PrintReversedMatrix(int[,] array)
+void PrintReversedMatrix(string[,] array)
 {
-    int maxNumberLength = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            maxNumberLength = Math.Max(maxNumberLength, array[i, j].ToString().Length);
-        }
-    }
-
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = array.GetLength(1) - 1; j >= 0; j--)
         {
             if (j == array.GetLength(1) - 1)
-                Console.Write("│ ");
-            Console.Write("{0," + maxNumberLength + "} ", array[i, j]);
+                Console.Write("│");
+            Console.Write(" {0,3} ", array[i, j]);
             if (j == 0)
                 Console.Write("│");
         }
@@ -89,24 +83,15 @@ void PrintReversedMatrix(int[,] array)
     }
 }
 
-void PrintReversedMatrix2(int[,] array)
+void PrintReversedMatrix2(string[,] array)
 {
-    int maxNumberLength = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            maxNumberLength = Math.Max(maxNumberLength, array[i, j].ToString().Length);
-        }
-    }
-
     for (int i = array.GetLength(0) - 1; i >= 0; i--)
     {
         for (int j = array.GetLength(1) - 1; j >= 0; j--)
         {
             if (j == array.GetLength(1) - 1)
-                Console.Write("│ ");
-            Console.Write("{0," + maxNumberLength + "} ", array[i, j]);
+                Console.Write("│");
+            Console.Write(" {0,3} ", array[i, j]);
             if (j == 0)
                 Console.Write("│");
         }
